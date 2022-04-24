a2c29d79f48ccddcf83ea148e602b58d343c7318
Murzik2020ai <61930598+Murzik2020ai@users.noreply.github.com>
2022-04-24 12:42:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+token ='5379923487:AAHkVvG5cX5fk-3UxSDg9rP2xtGWz43erec'
+'''
+t.me/smart_lang_bot.
 
-token ='xxx'
-
+Use this token to access the HTTP API:
+5379923487:AAHkVvG5cX5fk-3UxSDg9rP2xtGWz43erec
+'''
